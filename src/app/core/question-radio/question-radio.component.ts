b7d5f9fa368e919e2eab5bb1528fe82da51dfc3c
vardaaviv <varda2000@gmail.com>
2019-01-21 08:31:36 +0200

    continue

--- src/app/core/question-radio/question-radio.component.ts
+++ src/app/core/question-radio/question-radio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../../shared/services/api.service';
 
 @Component({
   selector: 'question-radio',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionRadioComponent implements OnInit {
 
-  constructor() { }
+  public options: any[];
+  public element: string
+;  constructor(public rest:ApiService) { }
 
   ngOnInit() {
+  }
+
+  add(option: string): void {
+    this.options = undefined;
+    option = option.trim();
+    if (!option) { return; }
+
+
+    const newOption: any = { name } as Hero;
+    this.rest.add(newOption)
+      .subscribe(optionn => this.options.push(option));
   }
 
 }
